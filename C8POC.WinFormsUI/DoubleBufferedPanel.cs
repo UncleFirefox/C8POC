98f43e51c682e373377292c1249a7d6bfdda377a
Albert Rodríguez Franco <albertrfranco@gmail.com>
2013-02-14 23:58:50 +0100

    Graphic modules and microsecond resolution timer for opcode fetching

---
+++ C8POC.WinFormsUI/DoubleBufferedPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace C8POC.WinFormsUI
+{
+    class DoubleBufferedPanel : Panel
+    {
+        public DoubleBufferedPanel()
+        {
+            DoubleBuffered = true;
+        }
+    }
+}
