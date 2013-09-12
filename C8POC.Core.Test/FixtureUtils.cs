27b4319c421f0043046170b4fad9600d5143b58d
Albert Rodriguez <albertrfranco@gmail.com>
2013-09-13 00:42:06 +0200

    Added a couple of basic unit tests

---
+++ C8POC.Core.Test/FixtureUtils.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FixtureUtils.cs" company="AlFranco">
+//   Albert Rodriguez Franco 2013
+// </copyright>
+// <summary>
+//   Defines the FixtureUtils type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace C8POC.Core.Test
+{
+    using C8POC.Core.Domain.Entities;
+    using C8POC.Interfaces.Domain.Entities;
+
+    /// <summary>
+    /// The fixture utils.
+    /// </summary>
+    public class FixtureUtils
+    {
+        /// <summary>
+        /// Gets a default MachineState instance
+        /// </summary>
+        /// <returns>
+        /// A machine state instance
+        /// </returns>
+        public static IMachineState DefaultMachineState()
+        {
+            var result = new C8MachineState();
+            return result;
+        }
+    }
+}
